862983ebf5f6552d3935e818064e53368c2283ec
Minsoo Kim <min-soo.kim@outlook.kr>
2021-07-05 17:12:23 +0900

    Updated number of wonderful substrings

--- algorithms/bitmask/number_of_wonderful_substrings.py
+++ algorithms/bitmask/number_of_wonderful_substrings.py
@@ -3,10 +3,10 @@
 
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
-        status_cnt = [1] + [0] * 1024
+        accumulated = [1] + [0] * 1024
         cnt = idx = 0
         for c in word:
             idx ^= 1 << (ord(c) - ord('a'))
-            cnt += status_cnt[idx] + sum(status_cnt[idx ^ (1 << n)] for n in range(10))
-            status_cnt[idx] += 1
+            cnt += accumulated[idx] + sum(accumulated[idx ^ (1 << n)] for n in range(10))
+            accumulated[idx] += 1
         return cnt
