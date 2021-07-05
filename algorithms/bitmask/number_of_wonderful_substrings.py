aeb55fd6323ae9b2c78b7f5836e8ab17bb6d66f3
Minsoo Kim <min-soo.kim@outlook.kr>
2021-07-05 14:27:41 +0900

    https://leetcode.com/problems/number-of-wonderful-substrings/

--- algorithms/bitmask/number_of_wonderful_substrings.py
+++ algorithms/bitmask/number_of_wonderful_substrings.py
@@ -2,10 +2,10 @@
 
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
-        cnt_status = [1] + [0] * 1024
+        status_cnt = [1] + [0] * 1024
         cnt = idx = 0
         for c in word:
             idx ^= 1 << (ord(c) - ord('a'))
-            cnt += cnt_status[idx] + sum(cnt_status[idx ^ (1 << n)] for n in range(10))
-            cnt_status[idx] += 1
+            cnt += status_cnt[idx] + sum(status_cnt[idx ^ (1 << n)] for n in range(10))
+            status_cnt[idx] += 1
         return cnt
