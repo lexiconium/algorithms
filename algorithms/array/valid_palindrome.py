9bf56f94e8aa2619d23e61345c9a8b8fa1e86063
Minsoo Kim <min-soo.kim@outlook.kr>
2021-07-06 19:34:34 +0900

    Some files are re-classified to string directory

--- algorithms/array/valid_palindrome.py
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/valid-palindrome/
-
-# time complexity: O(n)
-
-import re
-
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        return (s := re.sub('[^a-z0-9]', '', s.lower())) == s[::-1]
