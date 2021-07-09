d6a84a2381daf86dc82eab92074f7b15d0d56a44
Minsoo Kim <min-soo.kim@outlook.kr>
2021-07-09 16:30:21 +0900

    .

--- algorithms/array/array_partition.py
+++
@@ -1,7 +0,0 @@
-# https://leetcode.com/problems/array-partition-i/
-
-# time complexity: O(nlog(n))
-
-class Solution:
-    def arrayPairSum(self, nums: list[int]) -> int:
-        return sum(sorted(nums)[::2])
