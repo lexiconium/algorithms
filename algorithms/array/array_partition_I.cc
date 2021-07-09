ef12c54407d3840e9c911bd1186882eead17ba83
Minsoo Kim <min-soo.kim@outlook.kr>
2021-07-09 16:29:54 +0900

    Updated array partition I

---
+++ algorithms/array/array_partition_I.cc
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/array-partition-i/
+
+// time complexity: O(nlog(n))
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i += 2)
+            sum += nums[i];
+        return sum;
+    }
+};
