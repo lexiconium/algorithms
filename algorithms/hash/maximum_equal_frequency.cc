1fed6df7c0734c2f9a057cc975c14100e256e571
Minsoo Kim <min-soo.kim@outlook.kr>
2021-07-24 17:04:28 +0900

    Added maximum equal frequency

--- algorithms/hash/maximum_equal_frequency.cc
+++ algorithms/hash/maximum_equal_frequency.cc
@@ -3,3 +3,25 @@
 
 // time complexity: O(n)
 
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxEqualFreq(vector<int> &nums) {
+        int len = nums.size(), prefix_len = 0;
+        vector<int> freq(1e5 + 1), cnt(len + 1);
+        for (int i = 1, n, remainder; i <= len; i++) {
+            n = nums[i - 1];
+            cnt[freq[n]++]--;
+            cnt[freq[n]]++;
+            if (freq[n] * cnt[freq[n]] == i && i < len)
+                prefix_len = i + 1;
+            remainder = i - freq[n] * cnt[freq[n]];
+            if (cnt[remainder] == 1 && (remainder == 1 || remainder == freq[n] + 1))
+                prefix_len = i;
+        }
+        return prefix_len;
+    }
+};
