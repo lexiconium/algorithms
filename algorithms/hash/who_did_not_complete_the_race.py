1028b66297286cd90490b4833afa8fdeb85be5a3
Minsoo Kim <min-soo.kim@outlook.kr>
2021-08-12 23:24:53 +0900

    Removed who did not complete the race

--- algorithms/hash/who_did_not_complete_the_race.py
+++
@@ -1,6 +0,0 @@
-# https://programmers.co.kr/learn/courses/30/lessons/42576
-
-from collections import Counter
-
-def solution(participant, completion):
-    return list(Counter(participant) - Counter(completion))[0]
