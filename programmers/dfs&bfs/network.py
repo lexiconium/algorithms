4102e645e982d8f3ffc05ca2da21eedb1f1c1fd4
Minsoo Kim <min-soo.kim@outlook.kr>
2021-09-30 01:06:57 +0900

    dfs&bfs/network work in progress

--- programmers/dfs&bfs/network.py
+++ programmers/dfs&bfs/network.py
@@ -1,20 +1,16 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43162
 
-def find_root(disjoint_set, node):
-    while node != disjoint_set[node]:
-        node = disjoint_set[node]
-    return node
-
 def solution(n, computers):
-    disjoint_set = [m for m in range(n)]
-    
-    for u, others in enumerate(computers):
-        root_u = find_root(disjoint_set, u)
-        for v, is_connected in enumerate(others):
-            if u == v or not is_connected:
+    dj_set = [m for m in range(n)]
+    for i, others in enumerate(computers):
+        for j, is_connected in enumerate(others):
+            if i == j or not is_connected:
                 continue
-            root_v = find_root(disjoint_set, v)
-            if root_u != root_v:
-                disjoint_set[root_v] = root_u
-    
-    return len(set(disjoint_set))
+
+            if dj_set[i] != dj_set[j]:
+                if dj_set[i] > dj_set[j]:
+                    dj_set[i] = dj_set[j]
+                else:
+                    dj_set[j] = dj_set[i]
+
+    return len(set(dj_set))
