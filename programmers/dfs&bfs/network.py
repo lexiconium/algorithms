fd11ebefd3ed94c922f77a41553c753e17638bb9
Minsoo Kim <min-soo.kim@outlook.kr>
2021-10-02 23:41:45 +0900

    dfs&bfs/network added

--- programmers/dfs&bfs/network.py
+++ programmers/dfs&bfs/network.py
@@ -1,16 +1,17 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43162
 
 def solution(n, computers):
-    dj_set = [m for m in range(n)]
-    for i, others in enumerate(computers):
-        for j, is_connected in enumerate(others):
-            if i == j or not is_connected:
-                continue
-
-            if dj_set[i] != dj_set[j]:
-                if dj_set[i] > dj_set[j]:
-                    dj_set[i] = dj_set[j]
-                else:
-                    dj_set[j] = dj_set[i]
-
-    return len(set(dj_set))
+    cnt, visited = 0, 0
+    for i in range(len(computers)):
+        if visited & (1 << i):
+            continue
+        stk = [i]
+        while stk:
+            curr = stk.pop()
+            visited |= (1 << curr)
+            for neighbor, is_connected in enumerate(computers[curr]):
+                if not is_connected or visited & (1 << neighbor):
+                    continue
+                stk.append(neighbor)
+        cnt += 1
+    return cnt
