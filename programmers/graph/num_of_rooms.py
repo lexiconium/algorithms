2e5f909d5969d9054d6aa3189d08637329371a7f
Minsoo Kim <min-soo.kim@outlook.kr>
2021-11-30 21:32:36 +0900

    graph/num_of_rooms

--- programmers/graph/num_of_rooms.py
+++ programmers/graph/num_of_rooms.py
@@ -1,36 +1,32 @@
 # https://programmers.co.kr/learn/courses/30/lessons/49190
-# https://bellog.tistory.com/147
 
-from collections import defaultdict, deque
-
+from collections import defaultdict
 
 def solution(arrows):
-    dp = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-
-    q = deque([(0, 0)])
-    for d in arrows:
-        for _ in range(2):
-            x, y = q[-1]
-            dx, dy = dp[d]
-            q.append((x + dx, y + dy))
-
+    dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+    dy = [0, 1, 1, 1, 0, -1, -1, -1]
+    
     visited = defaultdict(lambda: False)
     visited_from = defaultdict(set)
-    cnt = 0
-
-    prev = q.popleft()
+    
+    prev = (0, 0)
     visited[prev] = True
-    while q:
-        curr = q.popleft()
-        if visited[curr]:
-            if prev not in visited_from[curr]:
-                cnt += 1
-        else:
-            visited[curr] = True
-
-        visited_from[curr].add(prev)
-        visited_from[prev].add(curr)
-
-        prev = curr
-
+    
+    cnt = 0
+    for d in arrows:
+        for _ in range(2):
+            x, y = prev
+            curr = (x + dx[d], y + dy[d])
+            
+            if visited[curr]:
+                if prev not in visited_from[curr]:
+                    cnt += 1
+            else:
+                visited[curr] = True
+            
+            visited_from[curr].add(prev)
+            visited_from[prev].add(curr)
+            
+            prev = curr
+    
     return cnt
