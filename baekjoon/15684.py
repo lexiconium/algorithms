3caf0f8f248b715078c63b593bf661539c294bc2
Minsoo Kim <min-soo.kim@outlook.kr>
2022-04-19 19:13:02 +0900

    사다리 조작

--- baekjoon/15684.py
+++ baekjoon/15684.py
@@ -12,7 +12,7 @@ class LadderGame:
     ladder: list[bool] = field(init=False)
 
     def __post_init__(self):
-        ladder = [False for _ in range((self.r + 1) * 10 + self.c + 1)]
+        ladder = [False for _ in range(10 * (self.r + 1) + self.c + 1)]
         for r, c in self.connections:
             ladder[10 * r + c] = True
         self.ladder = ladder
@@ -26,9 +26,10 @@ class LadderGame:
         return c
 
     def is_identity(self):
-        return all(
-            self.ends_if_starts_from(c) == c for c in range(1, self.c + 1)
-        )
+        for c in range(1, self.c + 1):
+            if self.ends_if_starts_from(c) != c:
+                return False
+        return True
 
     def can_be_identity(self, start: int = 11, num_connections_added: int = 0):
         if self.is_identity():
@@ -40,17 +41,22 @@ class LadderGame:
 
         min_added = 4
         for connection in range(start, 10 * self.r + self.c):
-            if self.ladder[connection]:
+            if (
+                self.ladder[connection - 1]
+                or self.ladder[connection]
+                or self.ladder[connection + 1]
+            ):
                 continue
+
             self.ladder[connection] = True
             _num_connections_added = self.can_be_identity(
                 start=connection + 1,
                 num_connections_added=num_connections_added + 1,
             )
+            self.ladder[connection] = False
+
             if _num_connections_added > -1:
                 min_added = min(min_added, _num_connections_added)
-
-            self.ladder[connection] = False
 
         return -1 if min_added == 4 else min_added
 
