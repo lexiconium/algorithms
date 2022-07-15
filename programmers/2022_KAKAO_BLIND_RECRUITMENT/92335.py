0f8f9a8f4e7aa1d292be1c8d2e0ef72c9033cdb3
Minsoo Kim <min-soo.kim@outlook.kr>
2022-07-15 18:02:42 +0900

    k진수에서 소수 개수 구하기

---
+++ programmers/2022_KAKAO_BLIND_RECRUITMENT/92335.py
@@ -0,0 +1,37 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/92335
+
+
+def convert(n: int, k: int):
+    if k == 10:
+        return n
+
+    digits = []
+    while n >= k:
+        n, digit = divmod(n, k)
+        digits.append(str(digit))
+
+    digits.append(str(n))
+
+    return int("".join(digits[::-1]))
+
+
+def is_prime(n: int):
+    if n == 1:
+        return False
+
+    print(n)
+
+    for denominator in range(2, int(n ** 0.5) + 1):
+        if n % denominator == 0:
+            return False
+
+    return True
+
+
+def solution(n, k):
+    return sum(
+        is_prime(num)
+        for num in map(
+            int, filter(lambda num: len(num), str(convert(n, k)).split("0"))
+        )
+    )
