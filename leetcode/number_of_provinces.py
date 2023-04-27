0875a265e65e2fb6f737a58dafc078d75d482c64
Minsoo Kim <min-soo.kim@outlook.kr>
2023-04-27 18:26:42 +0900

    number of provinces

--- leetcode/number_of_provinces.py
+++ leetcode/number_of_provinces.py
@@ -8,17 +8,17 @@ class Solution:
         def find(i: int) -> int:
             while disjoint_set[i] != i:
                 i = disjoint_set[i]
-            
+
             return i
 
         for i in range(len(disjoint_set) - 1):
             for j in range(i + 1, len(disjoint_set)):
                 if not is_connected[i][j]:
                     continue
-                
+
                 if (root_i := find(i)) == (root_j := find(j)):
                     continue
-                
+
                 disjoint_set[root_j] = root_i
 
         return sum(disjoint_set[i] == i for i in range(len(disjoint_set)))
