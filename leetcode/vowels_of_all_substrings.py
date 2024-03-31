ba8480ea07b40688fa77790532417af37b5abdf7
Minsoo Kim <min-soo.kim@outlook.kr>
2024-03-31 15:33:23 +0900

    dp

---
+++ leetcode/vowels_of_all_substrings.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/vowels-of-all-substrings/description/
+
+
+class Solution:
+    def countVowels(self, word: str) -> int:
+        return sum(
+            (i + 1) * (len(word) - i) for i, c in enumerate(word) if c in "aeiou"
+        )
